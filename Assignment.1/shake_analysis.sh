f0e48c3e520bc7bfc2c6ebf4586f4beb5f1ac51e
hananabousaleh <hananabousaleh@yahoo.ca>
2018-01-22 18:20:04 +0000

    Assignment 1 stuff

--- Assignment.1/shake_analysis.sh
+++ Assignment.1/shake_analysis.sh
@@ -1,10 +1,16 @@
 # How many lines in the text?
-wc -l shakespeare-macbeth-46.txt
+w1=$(wc -l < shakespeare-macbeth-46.txt)
+w2=$(($w1 + 1))
+echo $w2
 
 # How many lines contain the string LADY MACBETH?
-grep -c 'LADY MACBETH' shakespeare-macbeth-46.txt
+x=$(grep -c 'LADY MACBETH' shakespeare-macbeth-46.txt)
+echo $x
 
 # How many lines contain both strings LADY MACBETH and must?
-grep -c 'LADYMACBETH|must' shakespeare-macbeth-46.txt
+y=$(grep -c 'LADY MACBETH.*must\|must.*LADY MACBETH' shakespeare-macbeth-46.txt)
+echo $y
 
 # What is the 8th word in the line containing strings LADY MACBETH and blood?
+z=$(grep 'LADY MACBETH.*blood\|blood.*LADY MACBETH' shakespeare-macbeth-46.txt)
+echo $z
