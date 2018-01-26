00d6390ad7b20f00d8df1fc34f2bcd24a50b967e
hananabousaleh <hananabousaleh@yahoo.ca>
2018-01-26 02:31:55 +0000

    finished everything except 2.3

--- Assignment.1/integrate.py
+++ Assignment.1/integrate.py
@@ -1,4 +1,9 @@
+import functions as f
 import numpy as np
 
 def sym_gauss_int_sqr(xb, n=10):
-    return
+    y=linspace(-xb,xb,n)
+    trapezoids = 0
+    for i in y:
+        trapezoids +=((1/n)*((f.gauss(i)+f.gauss(i-1))/2))
+    return trapezoids**2
